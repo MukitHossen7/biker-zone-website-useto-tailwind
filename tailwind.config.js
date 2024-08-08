9d9461dca0898ca8256a404c12739a2c64ef36ae
MukitHossen7 <hossenmukit7@gmail.com>
2024-08-08 17:38:17 +0600

    header section done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      color: {
+        primary: "#E76F51",
+      },
+    },
+  },
+  plugins: [],
+};
